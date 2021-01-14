16fa69fc2b78d8609361281d581c1600aa20653f
samlv99 <samlv3112@gmail.com>
2021-01-15 00:16:33 +0700

    first commit

--- frontend/src/pages/Product/index.jsx
+++ frontend/src/pages/Product/index.jsx
@@ -58,34 +58,114 @@ function Product(props) {
             </Grid>
             <Grid item xs={12} sm={4} md={3}>
                 <BookCard 
-                title="Nhà Lãnh Đạo Không Chức Danh  " 
-                subtitle="Robin Sharma"
-                imgSrc="https://salt.tikicdn.com/cache/280x280/media/catalog/product/n/h/nhalanhdao.u2769.d20170307.t090846.484463.jpg"
-                description="80.000 ₫ -35%"
+                title="Không Gia Đình  " 
+                subtitle="Hector Malot"
+                imgSrc="https://salt.tikicdn.com/cache/280x280/ts/product/4a/c0/ee/c73cca3fbd5f8c9aefe98102acd633b5.jpg"
+                description="84.480đ -36%"
                 /> 
             </Grid>
             <Grid item xs={12} sm={4} md={3}>
                 <BookCard 
-                title="Nhà Lãnh Đạo Không Chức Danh  " 
-                subtitle="Robin Sharma"
-                imgSrc="https://salt.tikicdn.com/cache/280x280/media/catalog/product/n/h/nhalanhdao.u2769.d20170307.t090846.484463.jpg"
-                description="80.000 ₫ -35%"
+                title="Thép Đã Tôi Thế Đấy  " 
+                subtitle="Nikolai A.Ostrovsky"
+                imgSrc="https://salt.tikicdn.com/cache/280x280/media/catalog/product/2/d/2d-thep-da-toi-the-day.jpg"
+                description="116.101đ -10%"
                 /> 
             </Grid>
             <Grid item xs={12} sm={4} md={3}>
                 <BookCard 
-                title="Nhà Lãnh Đạo Không Chức Danh  " 
-                subtitle="Robin Sharma"
-                imgSrc="https://salt.tikicdn.com/cache/280x280/media/catalog/product/n/h/nhalanhdao.u2769.d20170307.t090846.484463.jpg"
-                description="80.000 ₫ -35%"
+                title="Gastby Vĩ Đại  " 
+                subtitle="F.Scott Fitzgerald"
+                imgSrc="https://salt.tikicdn.com/cache/280x280/media/catalog/product/g/a/gatsby_vi_dai_in-01_2.jpg"
+                description="114.750đ -15%"
                 /> 
             </Grid>
             <Grid item xs={12} sm={4} md={3}>
                 <BookCard 
-                title="Nhà Lãnh Đạo Không Chức Danh  " 
+                title="Tiếng Gọi Nơi Hoang Dã  " 
+                subtitle="Jack London"
+                imgSrc="https://salt.tikicdn.com/cache/280x280/media/catalog/product/t/i/tieng-goi-noi-hoang-da.u335.d20160607.t101621.jpg"
+                description="49.680đ -31%"
+                /> 
+            </Grid>
+            <Grid item xs={12} sm={4} md={3}>
+                <BookCard 
+                title="Muôn Kiếp Nhân Sinh  " 
+                subtitle="Nguyên Phong"
+                imgSrc="https://salt.tikicdn.com/cache/280x280/ts/product/30/ee/5a/e2aed009bb558b5d2cfbbe157b428ba4.jpg"
+                description="117.600đ -30%"
+                /> 
+            </Grid>
+            <Grid item xs={12} sm={4} md={3}>
+                <BookCard 
+                title="Tôi Từng Nghĩ Mọi Thứ Sẽ Ổn Khi Trở Thành Người Lớn  " 
+                subtitle="Kim Haenam"
+                imgSrc="https://salt.tikicdn.com/cache/280x280/ts/product/0b/e4/9d/9819a2afb854e301ebaf93e8e4a8a9d3.jpg"
+                description="76.801đ -20%"
+                /> 
+            </Grid>
+            <Grid item xs={12} sm={4} md={3}>
+                <BookCard 
+                title="Đừng Chạy Theo Số Đông  " 
+                subtitle="Kiên Trần"
+                imgSrc="https://salt.tikicdn.com/cache/280x280/ts/product/95/c5/71/f260011660001af960c42d7d2f1c5dc1.jpg"
+                description="130.000đ -35%"
+                /> 
+            </Grid>
+            <Grid item xs={12} sm={4} md={3}>
+                <BookCard 
+                title="Sống Thực Tế Giữa Đời Thực Dụng  " 
+                subtitle="Mễ Mông"
+                imgSrc="https://salt.tikicdn.com/cache/280x280/ts/product/25/d6/2c/f88080bba78a779fb78e1b76b73a9813.jpg"
+                description="49.000đ -50%"
+                /> 
+            </Grid>
+            <Grid item xs={12} sm={4} md={3}>
+                <BookCard 
+                title="Từ Tốt Đến Vĩ Đại  " 
+                subtitle="Jim Collins"
+                imgSrc="https://salt.tikicdn.com/cache/280x280/media/catalog/product/n/x/nxbtre_full_06372017_103735.u547.d20170117.t105220.139884.jpg"
+                description="77.050đ -33%"
+                /> 
+            </Grid>
+            <Grid item xs={12} sm={4} md={3}>
+                <BookCard 
+                title="5 Centimet Trên Giây  " 
+                subtitle="Shinkai Makoto"
+                imgSrc="https://salt.tikicdn.com/cache/280x280/media/catalog/product/i/m/img894.u3059.d20170616.t102330.519090.jpg"
+                description="30.000đ -40%"
+                /> 
+            </Grid>
+            <Grid item xs={12} sm={4} md={3}>
+                <BookCard 
+                title="Tôi Tài Giỏi - Bạn Cũng Thế " 
+                subtitle="Adam Khoo"
+                imgSrc="https://salt.tikicdn.com/cache/280x280/ts/product/8c/c5/bd/e7b36f74cc3c42c1fef5b6770e0e6d83.jpg"
+                description="75.520đ -36%"
+                /> 
+            </Grid>
+            <Grid item xs={12} sm={4} md={3}>
+                <BookCard 
+                title="Hành Trình Về Phương Đông  " 
+                subtitle="Nguyên Phong"
+                imgSrc="https://salt.tikicdn.com/cache/280x280/ts/product/46/9d/88/6560a409f40c37ac63d4e279bbea34b7.jpg"
+                description="57.200đ -35%"
+                /> 
+            </Grid>
+            <Grid item xs={12} sm={4} md={3}>
+                <BookCard 
+                title="Tuổi Trẻ Đáng Giá Bao Nhiêu  " 
+                subtitle="Rosie Nguyễn"
+                imgSrc="https://salt.tikicdn.com/cache/280x280/ts/product/f8/34/c8/5fbf2a6f9d07b76d98e2fff2b29b0d3c.jpg"
+                description="52.000đ -35%"
+                /> 
+            </Grid>
+            <Grid item xs={12} sm={4} md={3}>
+                <BookCard 
+                title="Ba Người Thầy Vĩ Đại  " 
                 subtitle="Robin Sharma"
-                imgSrc="https://salt.tikicdn.com/cache/280x280/media/catalog/product/n/h/nhalanhdao.u2769.d20170307.t090846.484463.jpg"
-                description="80.000 ₫ -35%"
+                imgSrc="https://salt.tikicdn.com/cache/280x280/ts/product/38/21/47/eb00fd978105557112ea749271d93cd8.jpg"
+                description="54.151đ -43%"
                 /> 
             </Grid>
         </Grid>
